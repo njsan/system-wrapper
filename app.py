edda2ecc8c181be5a22d57704ee8e2dd8efb1bf5
njsan <howe.jonathan@gmail.com>
2018-09-08 12:09:39 +0100

    added helper function for handling ext_id requirements

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
-import time, celeryconfig
+import time
+import celeryconfig
+import jsonschema
 from flask import Flask, jsonify, request
 from celery import Celery
 from subprocess import Popen, PIPE
-import jsonschema
 from flask_jsonschema_validator import JSONSchemaValidator
+from helper import redis_exid_register
+
 
 celery = Celery()
 celery.config_from_object(celeryconfig)
 
 
 @celery.task(bind=True, queue='celery')
-def add(self, x, y):
-    cmd = [x, y]
-    meta = {'cmd': x, 'arg': y}
+def add(self, x, y, z, q):
+    cmd = [x, y, z]
+    meta = {'cmd': x, 'argy': y, 'argz': z, 'extid': q}
     p = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    self.update_state(state='RUNNING', meta={'cmd': cmd, 'arg': y})
-    time.sleep(60)
+    self.update_state(state='RUNNING', meta={'cmd': cmd, 'argy': y, 'argz': z, 'extid': q})
+    time.sleep(5)
     stdout, stderr = p.communicate()
     output = {'stderr': stderr, 'stdout': stdout}
-    return (
-     meta, output)
+    return (meta, output)
 
 
 app = Flask(__name__)
@@ -29,10 +31,13 @@ JSONSchemaValidator(app=app, root='./')
 @app.route('/longtask', methods=['POST'])
 @app.validate('schema', 'register')
 def long_task():
+    ext_id = request.headers.get('X-Header-id')
     data = request.get_json()
     x = data['argx']
     y = data['argy']
-    task = add.delay(x, y).id
+    z = data['argz']
+    task = add.delay(x, y, z, ext_id).id
+    redis_exid_register(ext_id, task)
     return jsonify(task)
 
 
--- celeryconfig.py
+++ celeryconfig.py
@@ -7,4 +7,6 @@ imports = ('celery.task',)
 ## Using the database to store task state and results.
 result_backend = 'redis://192.168.99.100:32768'
 
-task_annotations = {'task.add': {'rate_limit': '10/s'}}
+task_track_started = 'True'
+
+#task_annotations = {'task.add': {'rate_limit': '10/s'}}
